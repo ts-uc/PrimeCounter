254bf29b1e820189daa20370580930659609a485
ts-uc <s0@ts-uc.org>
2021-12-22 20:48:03 +0900

    added firebase functions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCNS3E58YFUHla8t0tsCpPcdUwZdoWwgaw",
+  authDomain: "primecounter-925a2.firebaseapp.com",
+  databaseURL: "https://primecounter-925a2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "primecounter-925a2",
+  storageBucket: "primecounter-925a2.appspot.com",
+  messagingSenderId: "659741177243",
+  appId: "1:659741177243:web:1ad0899493b72200ccdd3e",
+  measurementId: "G-72TTQ0V3GE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
   return (
     <div className="App">
