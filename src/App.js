d08064f8e0b10212f7d1bc1db3424946878dc45e
ts-uc <s0@ts-uc.org>
2021-12-22 15:40:41 +0900

    firebase対応化

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <div>Hello World!</div>
-          <div>{`REACT_APP_APIKEY:${process.env.REACT_APP_APIKEY}`}</div>
-          <div>{`REACT_APP_AUTHDOMAIN:${process.env.REACT_APP_AUTHDOMAIN}`}</div>
-          <div>{`REACT_APP_DATABASEURL:${process.env.REACT_APP_DATABASEURL}`}</div>
-          <div>{`REACT_APP_PROJECT_ID:${process.env.REACT_APP_PROJECT_ID}`}</div>
-          <div>{`REACT_APP_STORAGE_BUCKET:${process.env.REACT_APP_STORAGE_BUCKET}`}</div>
-          <div>{`REACT_APP_MESSAGING_SENDER_ID:${process.env.REACT_APP_MESSAGING_SENDER_ID}`}</div>
-          <div>{`REACT_APP_APP_ID:${process.env.REACT_APP_APP_ID}`}</div>
-          <div>{`REACT_APP_MEASUREMENT_ID:${process.env.REACT_APP_MEASUREMENT_ID}`}</div>
         </p>
       </header>
     </div>
