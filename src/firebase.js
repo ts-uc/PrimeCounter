7611fd2aead937b2ff388bb42f66904fb18e3e8f
ts-uc <s0@ts-uc.org>
2021-12-25 09:09:52 +0900

    added firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,14 +11,19 @@ import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
   apiKey: "AIzaSyCNS3E58YFUHla8t0tsCpPcdUwZdoWwgaw",
   authDomain: "primecounter-925a2.firebaseapp.com",
-  databaseURL: "https://primecounter-925a2-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://primecounter-925a2-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "primecounter-925a2",
   storageBucket: "primecounter-925a2.appspot.com",
   messagingSenderId: "659741177243",
   appId: "1:659741177243:web:1ad0899493b72200ccdd3e",
-  measurementId: "G-72TTQ0V3GE"
+  measurementId: "G-72TTQ0V3GE",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export default analytics;
+export default db;
